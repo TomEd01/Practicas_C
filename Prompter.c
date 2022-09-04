c277fda5827ff8b94344f5d2f58ec22b060fe7e4
TomiCat <l21530369@cancun.tecnm.mx>
2022-09-04 13:45:06 -0500

    Agragando apuntador

--- Prompter.c
+++ Prompter.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-void func(int y){
-    ++y; //Incrementa el valor de y
-    printf("%d\n", y); //Imprime 6
+void func(int *py){
+    ++(*py);
+    printf("%d\n", *py); //Imprime 6
 }
 void main(){
     int x = 5; //Se inicializa la variable
     printf("%d\n", x); //Imprime 5
-    func(x); //Invoca func
-    printf("%d\n", x); //Imprime 5
+    func(&x); //Invoca func con el valor del puntero
+    printf("%d\n", x); //Imprime 6
 }
