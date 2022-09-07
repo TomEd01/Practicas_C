a98ac7bb4087d585b00564716cc504d39b746d58
TomiCat <l21530369@cancun.tecnm.mx>
2022-09-07 01:00:49 -0500

    Cuadrilatero con clases

--- POO/ejer1.cpp
+++ POO/ejer1.cpp
@@ -13,9 +13,14 @@ class Cuadrilatero{
     }
 };
 int main(){
-    float bas,alt;int i;
-    cin >> bas >> alt;
-    Cuadrilatero cu(bas,alt);
-    cout << "El area " << cu.getArea() << "\n";
+    float b,a;int i;
+    Cuadrilatero *cua = new Cuadrilatero[3];
+    for(i=0;i<3;i++){
+        cin >> b >> a;
+        cua[i] = Cuadrilatero(b,a);
+    }
+    for(i=0;i<3;i++){
+        cout << "Area del cuadrilatero: " << (i+1) << ": " << cua[i].getArea() << '\n';
+    }
     return 0;
 }
