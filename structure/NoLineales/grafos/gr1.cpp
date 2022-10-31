271be5d962ac2c991b73777e98b418d9cb9aa6ac
TomEd01 <l21530369@cancun.tecnm.mx>
2022-10-30 23:16:16 -0500

    Añadiendo recorrido por profundidad

--- structure/NoLineales/grafos/gr1.cpp
+++ structure/NoLineales/grafos/gr1.cpp
@@ -1,5 +1,6 @@
 #include <bits/stdc++.h>
 #include <queue>
+#include <stack>
 using namespace std;
 const int n=8;
 bool visitado[n]{false};
@@ -22,6 +23,25 @@ void recorridoAnchura(int m[n][n], int ni){
         }
     }
 }
+void profundidad(int m[n][n], int np){
+    stack <int> pila;
+    int c;
+    pila.push(np);
+    visitado[np]={true};
+    while(!pila.empty()){
+        np=pila.top();
+        pila.pop();
+        cout<<np<<"\n";
+        for(c=0;c<n;c++){
+            if(m[np][c]!=0 && m[np][c]!=INT_MAX){
+                if(visitado[c]!=true){
+					visitado[c]=true;
+                	pila.push(c);
+                }
+            }
+        }
+    }
+}
 int main(){
     int matriz[n][n]={
         {0,7,10,4,INT_MAX,INT_MAX,INT_MAX,INT_MAX},
@@ -33,6 +53,7 @@ int main(){
         {INT_MAX,INT_MAX,INT_MAX,1,INT_MAX,50,0,26},
         {INT_MAX,INT_MAX,INT_MAX,20,9,26,0}
     };
-    recorridoAnchura(matriz,3);
+    //recorridoAnchura(matriz,3);
+    profundidad(matriz,0);
     return 0;
 }
